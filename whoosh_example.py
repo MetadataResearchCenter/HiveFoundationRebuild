d546ffaaf3aa683e55ec0fcf1eaace814bc9112e
Robert Cheng <chengchang8@gmail.com>
2016-02-24 02:06:22 -0500

    counter done

--- whoosh_example.py
+++ whoosh_example.py
@@ -6,11 +6,15 @@ from whoosh.analysis import RegexTokenizer
 from collections import Counter
 
 
+#=============Input===========
+# location = input('Enter the file name: ')
+# print location
+
 #=============Index===========
 my_schema = Schema(id = ID(unique=True, stored=True), 
                    path = ID(stored=True), 
                    source = ID(stored=True),
-                   # author = TEXT(stored=True),
+                   author = TEXT(stored=True),
                    title = TEXT(stored=True),
                    text = TEXT)
 
@@ -25,22 +29,7 @@ writer.add_document(id = u'material1',
                     source = u'material1.txt',
                     title = u'Voltage Scaling of Graphene Device on SrTiO3 Epitaxial Thin Film',
                     text = io.open('sample/material1.txt', encoding='utf-8').read())
-#
-# writer.add_document(id = u'material2',
-#                     path = u'sample/material2.txt',
-#                     source = u'material2.txt',
-#                     title = u'Eutectic Growth in Two-Phase Multicomponent Alloys',
-#                     text = io.open('sample/material2.txt', encoding='utf-8').read())
-#
-# writer.add_document(id = u'material3',
-#                     path = u'sample/material3.txt',
-#                     source = u'material3.txt',
-#                     title = u'Short-range phase coherence and origin of the 1T-TiSe2 charge density wave',
-#                     text = io.open('sample/material3.txt', encoding='utf-8').read())
 writer.commit()
-
-
-
 
 #=============Searcher===========
 
@@ -55,26 +44,27 @@ with index.searcher() as s:
     results = s.search(q, terms = True)
 
 found = results.scored_length()
+#
+# if results.has_matched_terms():
+#     # What terms matched in the results?
+#     print(results.matched_terms())
+#
+#     # What terms matched in each hit?
+#     for hit in results:
+#         print(hit.matched_terms())
+#
+# if results.has_exact_length():
+#     print("Scored", found, "of exactly", len(results), "documents")
+# else:
+#     low = results.estimated_min_length()
+#     high = results.estimated_length()
+#
+#     print("Scored", found, "of between", low, "and", high, "documents")
 
-if results.has_matched_terms():
-    # What terms matched in the results?
-    print(results.matched_terms())
-
-    # What terms matched in each hit?
-    for hit in results:
-        print(hit.matched_terms())
-
-if results.has_exact_length():
-    print("Scored", found, "of exactly", len(results), "documents")
-else:
-    low = results.estimated_min_length()
-    high = results.estimated_length()
-
-    print("Scored", found, "of between", low, "and", high, "documents")
-
-#=============Searcher===========
+#=============Analyzer===========
 
 tokenizer = RegexTokenizer()
-
-for token in tokenizer(u"Hello world !"):
-    print repr(token.text)
+tokenList = []
+for token in tokenizer(io.open('sample/material1.txt', encoding='utf-8').read()):
+    tokenList.append(token.text)
+print Counter(tokenList)
