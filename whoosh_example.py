13665054e41f34467b5ec92a5d64add228023d56
Robert Cheng <chengchang8@gmail.com>
2016-02-24 01:32:15 -0500

    try3

--- whoosh_example.py
+++ whoosh_example.py
@@ -2,10 +2,9 @@ from whoosh.fields import Schema
 from whoosh.fields import ID, TEXT
 from whoosh.index import open_dir, create_in
 from whoosh.qparser import QueryParser
-from whoosh import scoring
-from whoosh.query import Term, And
 from whoosh.analysis import RegexTokenizer
-from whoosh import reading
+from collections import Counter
+
 
 #=============Index===========
 my_schema = Schema(id = ID(unique=True, stored=True), 
@@ -26,23 +25,24 @@ writer.add_document(id = u'material1',
                     source = u'material1.txt',
                     title = u'Voltage Scaling of Graphene Device on SrTiO3 Epitaxial Thin Film',
                     text = io.open('sample/material1.txt', encoding='utf-8').read())
-
-writer.add_document(id = u'material2',
-                    path = u'sample/material2.txt',
-                    source = u'material2.txt',
-                    title = u'Eutectic Growth in Two-Phase Multicomponent Alloys',
-                    text = io.open('sample/material2.txt', encoding='utf-8').read())
-
-writer.add_document(id = u'material3',
-                    path = u'sample/material3.txt',
-                    source = u'material3.txt',
-                    title = u'Short-range phase coherence and origin of the 1T-TiSe2 charge density wave',
-                    text = io.open('sample/material3.txt', encoding='utf-8').read())
+#
+# writer.add_document(id = u'material2',
+#                     path = u'sample/material2.txt',
+#                     source = u'material2.txt',
+#                     title = u'Eutectic Growth in Two-Phase Multicomponent Alloys',
+#                     text = io.open('sample/material2.txt', encoding='utf-8').read())
+#
+# writer.add_document(id = u'material3',
+#                     path = u'sample/material3.txt',
+#                     source = u'material3.txt',
+#                     title = u'Short-range phase coherence and origin of the 1T-TiSe2 charge density wave',
+#                     text = io.open('sample/material3.txt', encoding='utf-8').read())
 writer.commit()
 
 
 
-#=============Query===========
+
+#=============Searcher===========
 
 searcher = index.searcher()
 
@@ -71,3 +71,10 @@ else:
     high = results.estimated_length()
 
     print("Scored", found, "of between", low, "and", high, "documents")
+
+#=============Searcher===========
+
+tokenizer = RegexTokenizer()
+
+for token in tokenizer(u"Hello world !"):
+    print repr(token.text)
