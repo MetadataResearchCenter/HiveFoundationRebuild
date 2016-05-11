702e3aa14f77869fa9e5345982d5eaa4b0332ede
Robert Cheng <chengchang8@gmail.com>
2016-05-11 07:11:26 -0400

    counting still working

--- whoosh_revised.py
+++ whoosh_revised.py
@@ -1,7 +1,6 @@
 import os
 import sqlite3
-from nltk.corpus import stopwords
-from whoosh import scoring
+from whoosh.qparser import QueryParser
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.index import create_in
 
@@ -30,20 +29,20 @@ def add_doc(index, doc_loc):
     writer.commit()
 
 #=============getting all the stop words (unused, customized stop_word needed?)===========
-# def get_stop_word():
-#     stop_words = []
-#
-#     with open('stopwords_1.txt','r') as f1:
-#         for line in f1:
-#             for word in line.split():
-#                stop_words.append(word)
-#
-#     with open('stopwords_2.txt','r') as f2:
-#         for line in f2:
-#             for word in line.split():
-#                stop_words.append(word)
-#
-#     return stop_words
+def get_stop_word():
+    stop_words = []
+
+    with open('stopwords_1.txt','r') as f1:
+        for line in f1:
+            for word in line.split():
+               stop_words.append(word)
+
+    with open('stopwords_2.txt','r') as f2:
+        for line in f2:
+            for word in line.split():
+               stop_words.append(word)
+
+    return stop_words
 
 #=============getting UAT keywords from database===========
 db = None
@@ -65,17 +64,18 @@ def dbConnect():
     except sqlite3.Error as err:
         print('Error in connect:', err)
 
-def findConceptsLike():
+def findConceptsLike(term):
     prefLabels = []
 
-    sql = 'SELECT PrefLabel FROM CONCEPT'
+    sql = 'SELECT PrefLabel FROM CONCEPT WHERE PrefLabel LIKE' + "'%" + term + "%'"
+
     try:
         cursor = db.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()
         if len(rows) > 0:
             for r in rows:
-                prefLabels.append(r[0])
+                prefLabels.append(r)
         return prefLabels
     except sqlite3.IntegrityError as err:
         print('Integrity Error in getPrefLabelFor:', err)
@@ -84,27 +84,39 @@ def findConceptsLike():
     except sqlite3.Error as err:
         print('Error in getPrefLabelFor:', err)
 
-#=============searching and filtering stop words===========
-def searcher(index, thesaurus):
+#=============searching and counting===========
+def get_word_list(index):
     try:
         searcher = index.searcher()
-        word_list = list(searcher.lexicon("content"))
-        return word_list
+        return list(searcher.lexicon("content"))
     finally:
         searcher.close()
 
-def stop_word_filter(word_list):
-    filtered_list = filter(lambda x: x not in stopwords.words('english'), word_list)
-    return filtered_list
+def hitting(word_list):
+    result = {}
+    for w in word_list:
+        if findConceptsLike(w):
+            if w in result:
+                result[w] += 1
+            else:
+                result[w] = 1
+    return result
 
+def stop_word_filter(word_list, stop_word_list):
+    filtered_list = filter(lambda x: x not in stop_word_list, word_list)
+    return unicode(filtered_list)
 
 if __name__ == "__main__":
     index = create_index(my_schema)
     add_doc(index, "/Users/Robert/Desktop/HiveFoundationRebuild/sample/astronomyArticle.txt")
-    word_list = searcher(index)
-    filtered_content = stop_word_filter(word_list)
+    stop_word_list = get_stop_word()
+    word_list = get_word_list(index)
+    filtered_content = stop_word_filter(word_list, stop_word_list)
     dbConnect()
-    UAT_words = unicode(findConceptsLike())
+    # print hitting(filtered_content)
+    print (filtered_content)
+
+
 
 
 
